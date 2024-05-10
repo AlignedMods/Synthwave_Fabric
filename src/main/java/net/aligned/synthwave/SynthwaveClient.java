99bfa7e360c46d3b35ebd5a82ca5d28851d87d6e
AlignedMods <aligneddoesstuff@gmail.com>
2024-05-10 22:29:40 +0200

    Initial commit

---
+++ src/main/java/net/aligned/synthwave/SynthwaveClient.java
@@ -0,0 +1,10 @@
+package net.aligned.synthwave;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SynthwaveClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
